263891bfefe095ffc13b87395fb1a9a87dbe8077
Teresa Pelinski <teresapelinski@gmail.com>
2022-10-19 18:00:04 +0100

    Added test for torch dataaset and getters in the dataset class

--- dataset.py
+++ dataset.py
@@ -1,22 +1,42 @@
+from parser import sequence2st
 import torch
 from torch.utils.data import Dataset
 import numpy as np
 
 class forecastingDataset(Dataset):
     def __init__(self, data, seq_len):
-        self.seq_len = seq_len
+        self.__seq_len = seq_len
         
         # divide data into sequences of length sequence length
         num_sequences = int(np.ceil(data.shape[0] / self.seq_len))
         padding = int(num_sequences * self.seq_len - data.shape[0])
         
-        self.data = np.append(data, np.zeros((padding,8)), axis=0)
-        self.sequences = np.reshape(self.data, (num_sequences, self.seq_len, 8))
+        self.__data = np.append(data, np.zeros((padding,8)), axis=0)
+        self.__sequences = np.reshape(self.data, (num_sequences, self.seq_len, 8))
         
-        self.inputs = self.sequences[:-1] # (num_sequences-1, seq_length, num_sensors)
-        self.targets = self.sequences[1:] # (num_sequences-1, seq_length, num_sensors)
+        self.__inputs = self.sequences[:-1] # (num_sequences-1, seq_length, num_sensors)
+        self.__targets = self.sequences[1:] # (num_sequences-1, seq_length, num_sensors)
         
-        
+    @property
+    def seq_len(self):
+        return self.__seq_len
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def sequences(self):
+        return self.__sequences
+    
+    @property
+    def inputs(self):
+        return self.__inputs
+
+    @property    
+    def targets(self):
+        return self.__targets    
+    
     def __len__(self):
         return len(self.inputs)
     
